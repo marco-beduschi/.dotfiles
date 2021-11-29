f116e91b55b84cd742360b0166b6887c3014b320
Marco Beduschi <8469440+marco-beduschi@users.noreply.github.com>
2021-11-29 15:01:00 -0300

    docs: add TODO to setup_zsh.zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -3,7 +3,7 @@
 echo "\n<<< Starting ZSH Setup >>>\n"
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null # TODO: This command should not append to /etc/shells if command is path is already there.
 
 echo "Enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'
